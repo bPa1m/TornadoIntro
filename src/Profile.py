0b8d1ce82a47c9ac7d2fded4dcac906dafc75f01
bPa1m <Ben.p.palmieri@gmail.com>
2024-01-30 13:37:28 -0500

    HTML/CSS

--- src/Profile.py
+++ src/Profile.py
@@ -4,25 +4,29 @@ accountDatabase =  {
     "alice" : {
         "realname" : "Alice Smith",
         "dateOfBirth" : "Jan 1",
-        "email" : "alice@example.com"
+        "email" : "alice@example.com",
+        "img" : "/static/Alice_image.png"
     },
     
     "bob" : {
         "realname" : "Bob Jones",
         "dateOfBirth" : "Dec 31",
-        "email" : "bob@bob.xyz"
+        "email" : "bob@bob.xyz",
+        "img" : "/static/Bob_image.png"
     },
 
     "carol" : {
         "realname" : "Carol Ling",
         "dateOfBirth" : "July 17",
-        "email" : "carol@example.com"
+        "email" : "carol@example.com",
+        "img" : "/static/Carol_image.png"
     },
 
     "dave" : {
         "realname" : "Dave N. Port",
         "dateOfBirth" : "Mar 14",
-        "email" : "dave@dave.dave"
+        "email" : "dave@dave.dave",
+        "img" : "/static/Dave_image.png"
     }
 }
 
@@ -33,5 +37,6 @@ class Handler(tornado.web.RequestHandler):
         realname = accountDatabase[uname]["realname"]
         dob = accountDatabase[uname]["dateOfBirth"]
         email = accountDatabase[uname]["email"]
+        img = accountDatabase[uname]["img"]
 
-        self.render( "ProfileTemplate.html", uname = uname, rname = realname, dob = dob, email = email)
+        self.render( "ProfileTemplate.html", uname = uname, rname = realname, dob = dob, email = email, img = img)
